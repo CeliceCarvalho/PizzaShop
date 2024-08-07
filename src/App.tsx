0b694856b0adf87bbef35b50ec7962b6b00015e4
CeliceCarvalho22 <celicecarvalho22@gmail.com>
2024-08-06 20:07:14 -0400

    06/08 - casa

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './app/globals.css'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Toaster } from 'sonner';
+
+export function App() {
+  return (
+    <div>
+      <Toaster richColors />
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
